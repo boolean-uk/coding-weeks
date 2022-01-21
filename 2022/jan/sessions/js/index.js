16771176d0919fe15c8cbad9be6d9b1e8851f4ff
dearshrewdwit <dearshrewdwit@gmail.com>
2022-01-21 11:01:14 +0000

    add form, invert link colours

---
+++ 2022/jan/sessions/js/index.js
@@ -0,0 +1,13 @@
+function hi(name) {
+  return "Hi, " + name + "!"
+}
+//
+// function hi[name]() {
+//   return "Hi, [name]"
+// }
+
+hi("Ed")
+hi("Timmy")
+
+
+// separating what changes from what doesn't
