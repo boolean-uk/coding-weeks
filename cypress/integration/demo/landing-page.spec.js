94a8efffbe6b842d44e0b5c3129e7bc2c1f322da
dearshrewdwit <dearshrewdwit@gmail.com>
2022-08-08 09:47:53 +0100

    add coding week aug 22

--- cypress/integration/demo/landing-page.spec.js
+++ cypress/integration/demo/landing-page.spec.js
@@ -3,7 +3,7 @@ describe('Demo Coding Weekend', () => {
     cy.visit('/')
   })
 
-  it('displays a list the first item is the ', () => {
+  it('displays a list of sessions where the first item is 2022 Jan 22nd - 24th', () => {
     cy.get("ul li").first().should('have.text',"2022 Jan 22nd - 24th")
   })
 
@@ -11,5 +11,10 @@ describe('Demo Coding Weekend', () => {
     cy.get("ul li").first().click()
     cy.url().should('eq', Cypress.config().baseUrl + "/2022/jan")
     cy.get("ol li").first().should('have.text', "HTML & CSS")
+  })
+
+  it("has a link to boolean.co.uk", () => {
+    cy.get("ul li").last().click()
+    cy.url().should('eq', "https://boolean.co.uk/")
   })
 })
