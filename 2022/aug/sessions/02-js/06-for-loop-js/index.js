8ffdbdc93e6d1b63e5eac248128367b296b03f9a
Nathan King <nathanking3590@gmail.com>
2022-08-08 14:33:22 +0100

    add loops to js

---
+++ 2022/aug/sessions/02-js/06-for-loop-js/index.js
@@ -0,0 +1,35 @@
+// Counting from 1 to 10
+// for (initialization; condition; finalisation) {
+//   // code that runs repeatedly while the condition is true
+// }
+
+for (let i = 1; i <= 10; i++) {
+    console.log(i)
+}
+
+
+// --
+// Looking at every character in a string
+const firstName = 'Nathan'
+// console.log(firstName[0])
+// console.log(firstName[1])
+// console.log(firstName[2])
+
+for (let i = 0; i < firstName.length; i++) {
+    // console.log(firstName[0])
+    // console.log(i)
+    console.log(firstName[i])
+}
+
+
+// --
+// Look at every item in an array
+const hobbies = ['D&D', 'Music', 'Drawing', 'Coding']
+// console.log(hobbies[0])
+// console.log(hobbies[1])
+
+for (let i = 0; i < hobbies.length; i++) {
+    // console.log(hobbies[0])
+    // console.log(i)
+    console.log(hobbies[i])
+}
